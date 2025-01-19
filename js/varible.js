5ceaac10d9cff1a18d02b5b8317b26705056d1cc
Pratikkedar306 <129677345+Pratikkedar306@users.noreply.github.com>
2025-01-19 12:06:16 +0000

    config done

---
+++ js/varible.js
@@ -0,0 +1,9 @@
+const aId=125
+let aEmail="P@gmail.com"
+var aPass="1245"
+aLoc="Pune"
+ console.table([aId,aEmail,aLoc])
+aEmail="cj@gmail.com"
+aPass="145"
+aLoc ="Mumbai"
+console.table([aId,aEmail,aLoc])
